32e602e300ac6f5ebb31345ca86d7baaab377c1d
Nadira Hewabandu <nadira.hewabandu@unimelb.edu.au>
2019-04-02 15:21:29 +1100

    changes

--- src/components/steps/PreviewInfo.js
+++ src/components/steps/PreviewInfo.js
@@ -144,6 +144,25 @@ export default class PreviewInfo extends React.Component {
     }
   }
 
+  convertDateFormat(date) {
+    var formatDatestr = date;
+    // console.log( "year: "+ str.slice(0,4) )
+    // console.log( "mon: "+ str.slice(4,6) )
+    // console.log( "date: "+ str.slice(6,8) )
+
+    // formatDatestr = formatDatestr!=null ? formatDatestr : 0;
+    if (formatDatestr != null)
+      formatDatestr =
+        formatDatestr.slice(4, 6) +
+        "/" +
+        formatDatestr.slice(6, 8) +
+        "/" +
+        formatDatestr.slice(0, 4);
+    else formatDatestr = "N/A";
+
+    return formatDatestr;
+  }
+
   createCancerFields() {
     console.log("In new Cancer Preview");
     const tableRowStyle = {
@@ -179,7 +198,7 @@ export default class PreviewInfo extends React.Component {
           <tr>
             <td>Date Of Diagnosis</td>
             <td style={tableRowStyle}>
-              {values.dateOfDiagnosis /* {values.site.code} */}
+              {this.convertDateFormat(values.dateOfDiagnosis)} }
             </td>
           </tr>
           <tr>
@@ -271,14 +290,14 @@ export default class PreviewInfo extends React.Component {
               <tr>
                 <td>{val.column}</td>
                 <td style={tableRowStyle}>
-                  {val.previousVal}
-                  {/* {this.props.arrayEditedData[i].age} */}
+                  {val.column == "Date Of Diagnosis"
+                    ? this.convertDateFormat(val.previousVal)
+                    : val.previousVal}
                 </td>
                 <td style={tableRowStyle}>
-                  {val.newVal}
-
-                  {/* {this.props.changedParameters[i].age} */}
-                  {/* {values.id} */}
+                  {val.column == "Date Of Diagnosis"
+                    ? this.convertDateFormat(val.newVal)
+                    : val.newVal}
                 </td>
               </tr>
             ))}
